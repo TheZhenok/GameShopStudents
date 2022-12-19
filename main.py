afce3ec7377dacbde05ecd047314b480f227f287
Кайрат Смаков <@itstep.krg>
2022-12-19 21:20:12 +0600

    ADD: adding repository.py users

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from repository import Connection
 
 app = Flask(__name__)
 
-@app.route("/")
+conn = Connection()
+
+@app.route("/api/v1/main", methods=['GET', 'POST'])
 def index():
-    return jsonify("bingo")
+    if request.method == 'POST':
+        name = request.form.get("name")
+        lastname = request.form.get("lastname")
+        return jsonify({
+            "name": name,
+            "lastname": lastname
+        })
+    return jsonify({
+        "message": "Hello World!"
+    })
+
+@app.route("/api/v1/users", methods=['GET'])
+def users_list():
+    users = conn.users_list()
+    return jsonify(users)
+
+@app.route("/api/v1/users/save", methods=['POST'])
+def user_save():
+    user = request.get_json()
+    print(user)
+    return jsonify({
+        "message": "ok"
+    })
 
 if __name__ == '__main__':
     app.run(
